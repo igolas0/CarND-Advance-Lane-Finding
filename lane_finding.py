e1f5a9d7e999e1dd00670e6ff46c7f2b9acede6f
Igor Molina <canutito1@hotmail.com>
2017-04-12 06:18:46 +0200

    Updates to lane_finding.py. Line Class added with implementation of lane detection function.

--- lane_finding.py
+++ lane_finding.py
@@ -1,51 +1,46 @@
 import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
-import glob
 import cv2
 
 class Line():
-    def __init__(self):
+    def __init__(self, Mywindow_width, Mywindow_height, Mymargin, My_ym = 1, My_xm = 1, Mysmooth_fsacotr = 15):
         #list storing all past (left/right) center set values 
         self.recent_centers = [] 
         # the window pixel width of the center values used to count pixels inside center windwos to determine curve values
         self.window_width = Mywindow_width 
         # the window pixel height of the center calues used to count pixels inside center windows to dtermine curve values breaks the image into vertical levels
         self.window_heighth = Mywindow_heighth 
-        #polynomial coefficients averaged over the last n iterations
-        self.best_fit = None  
-        #polynomial coefficients for the most recent fit
-        self.current_fit = [np.array([False])]  
-        #radius of curvature of the line in some units
-        self.radius_of_curvature = None 
-        #distance in meters of vehicle center from the line
-        self.line_base_pos = None 
-        #difference in fit coefficients between last and new fits
-        self.diffs = np.array([0,0,0], dtype='float') 
-        #x values for detected line pixels
-        self.allx = None  
-        #y values for detected line pixels
-        self.ally = None
+        #pixel distance in both directions to slide (template for searching)
+        self.margin = MyMargin
 
-def find_window_centroids(image, window_width, window_height, margin):
+        self.ym_per_pix = My_ym # meters per pixel in vertical axis
+
+        self.xm_per_pix = My_xm # meters per pixel in horizontal axis
+
+        self.smooth_factor = Mysmooth_factor
+
+    #define function to find lane segment positions
+    def find_window_centroids(self, warped):
     
-    window_centroids = [] # Store the (left,right) window centroid positions per level
-    window = np.ones(window_width) # Create our window template that we will use for convolutions
+        window_width = self.window_width 
+        window_height = self.window_height 
+        margin = self.margin 
+
+        window_centroids = [] # store (left/right) window centroid positions per level
+        window = np.ones(window_width) # create template for convolutions
+        # First find the two starting positions for the left and right lane by using np.sum to get the vertical image slice
+        # and then np.convolve the vertical image slice with the window template 
     
-    # First find the two starting positions for the left and right lane by using np.sum to get the vertical image slice
-    # and then np.convolve the vertical image slice with the window template 
+        # Sum quarter bottom of image to get slice, could use a different ratio
+        l_sum = np.sum(warped[int(3*warped.shape[0]/4):,:int(warped.shape[1]/2)], axis=0)
+        l_center = np.argmax(np.convolve(window,l_sum))-window_width/2
+        r_sum = np.sum(warped[int(3*warped.shape[0]/4):,int(warped.shape[1]/2):], axis=0)
+        r_center = np.argmax(np.convolve(window,r_sum))-window_width/2+int(warped.shape[1]/2)
     
-    # Sum quarter bottom of image to get slice, could use a different ratio
-    l_sum = np.sum(warped[int(3*warped.shape[0]/4):,:int(warped.shape[1]/2)], axis=0)
-    l_center = np.argmax(np.convolve(window,l_sum))-window_width/2
-    r_sum = np.sum(warped[int(3*warped.shape[0]/4):,int(warped.shape[1]/2):], axis=0)
-    r_center = np.argmax(np.convolve(window,r_sum))-window_width/2+int(warped.shape[1]/2)
+        # Add what we found for the first layer
+        window_centroids.append((l_center,r_center))
     
-    # Add what we found for the first layer
-    window_centroids.append((l_center,r_center))
-    
-    # Go through each layer looking for max pixel locations
-    for level in range(1,(int)(warped.shape[0]/window_height)):
+        # Go through each layer looking for max pixel locations
+        for level in range(1,(int)(warped.shape[0]/window_height)):
 	    # convolve the window into the vertical slice of the image
 	    image_layer = np.sum(warped[int(warped.shape[0]-(level+1)*window_height):int(warped.shape[0]-level*window_height),:], axis=0)
 	    conv_signal = np.convolve(window, image_layer)
@@ -61,5 +56,6 @@ def find_window_centroids(image, window_width, window_height, margin):
 	    r_center = np.argmax(conv_signal[r_min_index:r_max_index])+r_min_index-offset
 	    # Add what we found for that layer
 	    window_centroids.append((l_center,r_center))
-
-    return window_centroidso
+        self.recent_centers.append(window_centroids)
+    # return averaged values of the line centers (averaged over no. of frames given by smooth factor).
+    return np.average(self.recent_centers[-self.smooth_factor:], axis = 0)
